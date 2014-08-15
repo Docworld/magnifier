112ff02e0406710e40f338953000a37594839f41
Ryan Kresse <rkresse53@gmail.com>
2014-08-15 16:52:36 -0400

    inital commit

---
+++ magnifier.js
@@ -0,0 +1,79 @@
+var magnifier = (function(magnifyingLevel, mouseOverImgWidth, imgPath, cursorWidth, cursorHeight) {
+
+  var mouseOverImgWidthWithPx = mouseOverImgWidth + "px";
+  var magnifiedImgWidth = (mouseOverImgWidth * magnifyingLevel) + "px";
+  
+  var pictureOffset = $('.mouseover').offset();
+  
+
+
+  
+  var displayMagnifiedImage = function (x, y){
+     "use strict;"
+    $('#magnified-img').css({"background-image": "url(" + imgPath + ")", "background-size": magnifiedImgWidth + " auto", "background-position": x + " " + y});
+  };
+
+ 
+  return {
+    
+    
+    getCoordinates: function(event) {
+    "use strict;"
+    var xCoord = -(event.pageX - pictureOffset.left);
+    var yCoord = -(event.pageY - pictureOffset.top); 
+    var bigXCoord = (xCoord * magnifyingLevel) + "px";
+    var bigYCoord = (yCoord * magnifyingLevel) + "px";
+    displayMagnifiedImage(bigXCoord, bigYCoord);
+  },
+
+
+    init: function(){
+      getCoordinates();
+    },  
+
+    hideMagnifiedImage: function () {
+      "use strict;"
+      $('#magnified-img').css({"background-image": "none"});
+    },
+
+    loadMouseOverImg: function () {
+      $('.mouseover').attr({
+        src: imgPath,
+        width: mouseOverImgWidthWithPx,
+        height: "auto"
+      });
+    },
+
+    setMagnifyingOutputSize: function () {
+        var magnifiedOutputWidth = (magnifyingLevel * cursorWidth) + "px";
+        var magnifiedOutputHeight = (magnifyingLevel * cursorHeight) + "px";
+        $('#magnified-img').css({"width": magnifiedOutputWidth, "height": magnifiedOutputHeight});
+    }
+
+  
+
+  };
+
+})(5, 400, "gnome-compressed.jpg", 32, 32);
+
+
+
+$('.mouseover').on( "mousemove", function( event ) {
+  magnifier.getCoordinates(event);
+
+});
+
+$('.mouseover').on( "mouseleave", function( event ) {
+  magnifier.hideMagnifiedImage();
+
+});
+
+(function () {
+  magnifier.loadMouseOverImg();
+  magnifier.setMagnifyingOutputSize();
+
+})();
+
+
+
+
