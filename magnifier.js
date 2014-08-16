0d7b4ab426273a6927f9f4047c8cca99cd18f074
Ryan Kresse <rkresse53@gmail.com>
2014-08-15 23:33:16 -0400

    cleaned up the code a bit

--- magnifier.js
+++ magnifier.js
@@ -1,82 +1,76 @@
-//h3llo yo
+(function ($, magnifyingLevel, mouseOverImgWidth, imgPath, cursorWidth, cursorHeight) {
 
-var magnifier = (function(magnifyingLevel, mouseOverImgWidth, imgPath, cursorWidth, cursorHeight) {
+  //some variable we'll need throughout.
 
-  var mouseOverImgWidthWithPx = mouseOverImgWidth + "px";
+  var mouseOverImgWidthWithPx = mouseOverImgWidth + "px"; 
   var magnifiedImgWidth = (mouseOverImgWidth * magnifyingLevel) + "px";
   
-  var pictureOffset = $('.mouseover').offset();
   
+  // This next variable stores the coordinates of our mouseover div in relation to the document. 
+  // We'll use it to calculate the position of our mouse within that div in our getCoordinates function.
+  var pictureOffset = $('.mouseover').offset(); 
+  
+  
+  function getCoordinates(event) {
+    "use strict;"
+    // the coordinates of the mouse in relation to the mouseover div.
+    var mouseXCoord = -(event.pageX - pictureOffset.left);  
+    var mouseYCoord = -(event.pageY - pictureOffset.top);
+
+    // The crucial bit. We take the coordinates of our mouse on the smaller image and multiply it by the
+    // magnifying level, giving us the corresponding coordinates on the 'magnified image'.
+    var bigImageXCoord = (mouseXCoord * magnifyingLevel) + "px";
+    var bigImageYCoord = (mouseYCoord * magnifyingLevel) + "px";
+    displayMagnifiedImage(bigImageXCoord, bigImageYCoord);
+  }
 
 
   
-  var displayMagnifiedImage = function (x, y){
+  function loadMouseOverImg () {
+    $('.mouseover').attr({
+      src: imgPath,
+      width: mouseOverImgWidthWithPx,
+      height: "auto"
+      });
+    };
+
+  function setMagnifyingOutputSize () {
+      var magnifiedOutputWidth = (magnifyingLevel * cursorWidth) + "px";
+      var magnifiedOutputHeight = (magnifyingLevel * cursorHeight) + "px";
+      $('#magnified-img').css({"width": magnifiedOutputWidth, "height": magnifiedOutputHeight});
+    };
+
+
+  function displayMagnifiedImage(x, y) {
      "use strict;"
     $('#magnified-img').css({"background-image": "url(" + imgPath + ")", "background-size": magnifiedImgWidth + " auto", "background-position": x + " " + y});
   };
 
- 
-  return {
-    
-    
-    getCoordinates: function(event) {
-    "use strict;"
-    var xCoord = -(event.pageX - pictureOffset.left);
-    var yCoord = -(event.pageY - pictureOffset.top); 
-    var bigXCoord = (xCoord * magnifyingLevel) + "px";
-    var bigYCoord = (yCoord * magnifyingLevel) + "px";
-    displayMagnifiedImage(bigXCoord, bigYCoord);
-  },
-
-
-    init: function(){
-      getCoordinates();
-    },  
-
-    hideMagnifiedImage: function () {
+  
+  function hideMagnifiedImage () {
       "use strict;"
       $('#magnified-img').css({"background-image": "none"});
-    },
-
-    loadMouseOverImg: function () {
-      $('.mouseover').attr({
-        src: imgPath,
-        width: mouseOverImgWidthWithPx,
-        height: "auto"
-      });
-    },
-
-    setMagnifyingOutputSize: function () {
-        var magnifiedOutputWidth = (magnifyingLevel * cursorWidth) + "px";
-        var magnifiedOutputHeight = (magnifyingLevel * cursorHeight) + "px";
-        $('#magnified-img').css({"width": magnifiedOutputWidth, "height": magnifiedOutputHeight});
     }
 
-  
-
-  };
+ 
 
 
+  $('.mouseover').on( "mousemove", function( event ) {
+    getCoordinates(event);
+  });
+
+  $('.mouseover').on( "mouseleave", function( event ) {
+    hideMagnifiedImage();
+  });
+
+
+  loadMouseOverImg();
+  setMagnifyingOutputSize();
 
 
 
-})(5, 400, "gnome.jpg", 32, 32);
+}(jQuery, 5, 400, "gnome.jpg", 32, 32));
 
-$('.mouseover').on( "mousemove", function( event ) {
-  magnifier.getCoordinates(event);
-
-});
-
-$('.mouseover').on( "mouseleave", function( event ) {
-  magnifier.hideMagnifiedImage();
-
-});
-
-(function () {
-  magnifier.loadMouseOverImg();
-  magnifier.setMagnifyingOutputSize();
-
-})();
 
 
 
